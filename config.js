ef9b4b26d873c351ab2fb850616ca6f09d1ee586
aboeidelian-create <aboeidelian@gmail.com>
2026-09-19 18:22:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// ضع هنا بيانات مشروع Supabase الخاص بك
+// Settings -> API في لوحة Supabase
+const SUPABASE_URL = "https://rzhveemqaczbeljwayvc.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aHZlZW1xYWN6YmVsandheXZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNjEzODEsImV4cCI6MjEwNDgzNzM4MX0.1pXUa4jbvzhBHKOOB5w3RXNWN0mtOhqiiyHTvvjuR_0";
